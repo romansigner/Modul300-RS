ff247b006e9db18dc04ba93d024d4ff7d98a8c32
romansigner <roman_signer@hotmail.com>
2019-03-21 15:03:52 +0100

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -6,12 +6,16 @@
 
 apt-get update -y
 apt-get -y install apache2
-apt install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php -y
+
+
+cp /vagrant/001-mysite.conf /etc/apache2/sites-available/
+a2ensite 001-mysite.conf
+a2enmod proxy
+a2enmod proxy_http
 service apache2 restart
 
-sudo a2enmod proxy
-sudo a2enmod proxy_html
-sudo a2enmod proxy_http
-sudo a2ensite 001-mysite.conf
-
-service apache2 restart
+#firewall einstellen
+apt-get install ufw
+sudo ufw enable
+sudo ufw allow 80/tcp
+sudo ufw allow from 192.168.43.214 to any port 22
